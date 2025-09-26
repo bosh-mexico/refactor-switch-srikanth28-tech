41283fcbe84b8d23ddab6e0231b0395870097e6d
srikanth28-tech <srikanthmsd47@gmail.com>
2025-09-26 09:51:50 +0530

    Create paypal_processor.java

---
+++ paypal_processor.java
@@ -0,0 +1,10 @@
+public class paypal_processor {
+  // PayPal Implementation
+  class PayPalProcessor implements PaymentProcessor {
+      public void process(double amount) {
+          System.out.println("Processing PayPal payment of $" + String.format("%.2f", amount));
+          // Add PayPal-specific logic here
+      }
+  }
+
+}
