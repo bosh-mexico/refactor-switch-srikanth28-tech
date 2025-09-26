ef441af3c6ee8c51f3eaef5454ae86ee45010c78
srikanth28-tech <srikanthmsd47@gmail.com>
2025-09-26 10:25:14 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,21 +1,12 @@
-// Main Checkout Demo
+// Main Application
 public class CheckoutDemo {
-    public static void checkout(String mode, double amount) {
-        PaymentProcessor processor = PaymentProcessorFactory.getProcessor(mode);
-        if (processor != null) {
-            processor.process(amount);
-        } else {
-            System.out.println("Invalid payment mode selected!");
-        }
-    }
-
     public static void main(String[] args) {
+        CheckoutService service = new CheckoutService();
         double amount = 150.75;
 
-        checkout("PAYPAL", amount);
-        checkout("GOOGLEPAY", amount);
-        checkout("CREDITCARD", amount);
-        checkout("BITCOIN", amount); // Unsupported mode
+        service.checkout("PAYPAL", amount);
+        service.checkout("GOOGLEPAY", amount);
+        service.checkout("CREDITCARD", amount);
+        service.checkout("BITCOIN", amount); // Unsupported
     }
 }
-
