fb93209df685f85e82b6093247525e9fce6704e3
srikanth28-tech <srikanthmsd47@gmail.com>
2025-09-29 19:09:03 +0530

    Update Creditcard_processor.java

--- Creditcard_processor.java
+++ Creditcard_processor.java
@@ -1,9 +1,7 @@
-  public class Creditcard_processor{
-      // CreditCard Implementation
-      class CreditCardProcessor implements PaymentProcessor {
-          public void process(double amount) {
-              System.out.println("Processing Credit Card payment of $" + String.format("%.2f", amount));
-              // Add Credit Card-specific logic here
-          }
+  // CreditCard Implementation 
+  class CreditCardProcessor implements PaymentProcessor {
+      public void process(double amount) {
+          System.out.println("Processing Credit Card payment of $" + String.format("%.2f", amount));
+          // Add Credit Card-specific logic here
       }
   }
